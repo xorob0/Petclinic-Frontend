8f6a9949a6a059c29a67cb93c916292aaa4b7790
Timothee Simon <xorob0@posteo.net>
2018-11-15 13:57:46 +0100

    Added real birthdate

--- src/components/PetsForm.js
+++ src/components/PetsForm.js
@@ -27,7 +27,7 @@ class AddPetsDialog extends React.Component {
   };
 
 	handleSubmit = (e) => {
-		const url = "http://localhost:8080/api/v1/petInsert?type=" + this.state.type + "&name=" + this.state.name + "&ownerFirstname=" + this.state.first + "&ownerLastname=" + this.state.last + "&birthdate=1932-11-11";
+		const url = "http://localhost:8080/api/v1/petInsert?type=" + this.state.type + "&name=" + this.state.name + "&ownerFirstname=" + this.state.first + "&ownerLastname=" + this.state.last + "&birthdate=" + this.state.birth;
 		fetch(url);
 		alert(this.state.name + " was successfully added to the database");
 	}
