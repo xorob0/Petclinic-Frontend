b42a457c847f621e55897da544d2afc2b0cc4be9
Timothee Simon <xorob0@posteo.net>
2018-11-29 11:35:27 +0100

    Added sorting and using new api format

--- src/components/Pets.js
+++ src/components/Pets.js
@@ -5,18 +5,129 @@ import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
+import PropTypes from 'prop-types';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Tooltip from '@material-ui/core/Tooltip';
 
-class Vets extends Component {
+function desc(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function stableSort(array, cmp) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = cmp(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map(el => el[0]);
+}
+
+function getSorting(order, orderBy) {
+  return order === 'desc' ? (a, b) => desc(a, b, orderBy) : (a, b) => -desc(a, b, orderBy);
+}
+
+const rows = [
+  { id: 'name', numeric: false, disablePadding: true, label: 'Name' },
+  { id: 'type', numeric: false, disablePadding: true, label: 'Type' },
+  { id: 'birthdate', numeric: true, disablePadding: false, label: 'Birthdate' },
+  { id: 'owner', numeric: true, disablePadding: false, label: 'Owner' },
+];
+
+class EnhancedTableHead extends React.Component {
+  createSortHandler = property => event => {
+    this.props.onRequestSort(event, property);
+  };
+
+  render() {
+    const { order, orderBy } = this.props;
+
+    return (
+      <TableHead>
+        <TableRow>
+          <TableCell>
+          </TableCell>
+          {rows.map(row => {
+            return (
+              <TableCell
+                key={row.id}
+                numeric={row.numeric}
+                padding={row.disablePadding ? 'none' : 'default'}
+                sortDirection={orderBy === row.id ? order : false}
+              >
+                <Tooltip
+                  title="Sort"
+                  placement={row.numeric ? 'bottom-end' : 'bottom-start'}
+                  enterDelay={300}
+                >
+                  <TableSortLabel
+                    active={orderBy === row.id}
+                    direction={order}
+                    onClick={this.createSortHandler(row.id)}
+                  >
+                    {row.label}
+                  </TableSortLabel>
+                </Tooltip>
+              </TableCell>
+            );
+          }, this)}
+        </TableRow>
+      </TableHead>
+    );
+  }
+}
+
+EnhancedTableHead.propTypes = {
+  onRequestSort: PropTypes.func.isRequired,
+  order: PropTypes.string.isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
+
+class Pets extends Component {
   constructor() {
     super();
     this.state = {
       rows: [],
+    order: 'asc',
+    orderBy: 'firstname',
+    page: 0,
+    rowsPerPage: 5,
     };
   }
 
-  componentWillMount() {
+  handleRequestSort = (event, property) => {
+    const orderBy = property;
+    let order = 'desc';
+
+    if (this.state.orderBy === property && this.state.order === 'desc') {
+      order = 'asc';
+    }
+
+    this.setState({ order, orderBy });
+  };
+
+  handleClick = (event, id) => {
+  };
+
+  handleChangePage = (event, page) => {
+    this.setState({ page });
+  };
+
+  handleChangeRowsPerPage = event => {
+    this.setState({ rowsPerPage: event.target.value });
+  };
+
+
+  getOwner(id) {
     var that = this;
-    const url = "http://localhost:9999/api/v1/pets";
+    const url = "http://localhost:9999/api/v1/owners?id=" + id;
 
     fetch(url)
       .then(function(response) {
@@ -26,33 +137,65 @@ class Vets extends Component {
         return response.json();
       })
       .then(function(data) {
-        that.setState({ rows: data });
+				return data[0].firstname
       });
   }
 
+  async componentDidMount() {
+    var that = this;
+    const url = "http://localhost:9999/api/v1/pets";
+
+    fetch(url)
+      .then(function(response) {
+        if (response.status >= 400) {
+          throw new Error("Bad response from server");
+        }
+        return response.json();
+			}).then(function(data) {
+				return data;
+
+			})
+      .then(function(data) {
+        that.setState({ rows: data });
+      });
+
+  }
+
   render() {
+
+    const {  order, orderBy, rowsPerPage, page } = this.state;
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
     return (
       <Paper>
         <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell numeric>Type</TableCell>
-              <TableCell numeric>Name</TableCell>
-              <TableCell numeric>Owner</TableCell>
-              <TableCell numeric>Birthdate</TableCell>
-            </TableRow>
-          </TableHead>
+					<EnhancedTableHead
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={this.handleRequestSort}
+              rowCount={rows.length}
+            />
           <TableBody>
-            {this.state.rows.map(row => {
-              return (
-                <TableRow key={row.id}>
-                  <TableCell numeric>{row.type}</TableCell>
-                  <TableCell numeric>{row.name}</TableCell>
-                  <TableCell numeric>{row.ownerFirstname} {row.ownerLastname}</TableCell>
-                  <TableCell numeric>{row.birthdate}</TableCell>
-                </TableRow>
-              );
-            })}
+						{stableSort(this.state.rows, getSorting(order, orderBy))
+                .map(n => {
+                  return (
+                    <TableRow
+                      hover
+                      onClick={event => this.handleClick(event, n.id)}
+                      role="checkbox"
+                      tabIndex={-1}
+                      key={n.id}
+                    >
+                      <TableCell padding="checkbox">
+                      </TableCell>
+                      <TableCell component="th" scope="row" padding="none">
+                        {n.name}
+                      </TableCell>
+                      <TableCell numeric>{n.type}</TableCell>
+                      <TableCell numeric>{n.birthdate}</TableCell>
+											<TableCell numeric>{n.ownerId}</TableCell>
+                    </TableRow>
+                  );
+                })}
           </TableBody>
         </Table>
       </Paper>
@@ -60,4 +203,4 @@ class Vets extends Component {
   }
 }
 
-export default Vets;
+export default Pets;
--- src/components/Vets.js
+++ src/components/Vets.js
@@ -5,14 +5,123 @@ import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
+import PropTypes from 'prop-types';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Tooltip from '@material-ui/core/Tooltip';
+
+function desc(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function stableSort(array, cmp) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = cmp(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map(el => el[0]);
+}
+
+function getSorting(order, orderBy) {
+  return order === 'desc' ? (a, b) => desc(a, b, orderBy) : (a, b) => -desc(a, b, orderBy);
+}
+
+const rows = [
+  { id: 'firstname', numeric: false, disablePadding: true, label: 'Firstname' },
+  { id: 'lastname', numeric: true, disablePadding: false, label: 'Lastname' },
+  { id: 'speciality', numeric: true, disablePadding: false, label: 'Speciality' },
+];
+
+class EnhancedTableHead extends React.Component {
+  createSortHandler = property => event => {
+    this.props.onRequestSort(event, property);
+  };
+
+  render() {
+    const { order, orderBy } = this.props;
+
+    return (
+      <TableHead>
+        <TableRow>
+          <TableCell>
+          </TableCell>
+          {rows.map(row => {
+            return (
+              <TableCell
+                key={row.id}
+                numeric={row.numeric}
+                padding={row.disablePadding ? 'none' : 'default'}
+                sortDirection={orderBy === row.id ? order : false}
+              >
+                <Tooltip
+                  title="Sort"
+                  placement={row.numeric ? 'bottom-end' : 'bottom-start'}
+                  enterDelay={300}
+                >
+                  <TableSortLabel
+                    active={orderBy === row.id}
+                    direction={order}
+                    onClick={this.createSortHandler(row.id)}
+                  >
+                    {row.label}
+                  </TableSortLabel>
+                </Tooltip>
+              </TableCell>
+            );
+          }, this)}
+        </TableRow>
+      </TableHead>
+    );
+  }
+}
+
+EnhancedTableHead.propTypes = {
+  onRequestSort: PropTypes.func.isRequired,
+  order: PropTypes.string.isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
 
 class Vets extends Component {
   constructor() {
     super();
     this.state = {
       rows: [],
+    order: 'asc',
+    orderBy: 'firstname',
+    page: 0,
+    rowsPerPage: 5,
     };
   }
+
+  handleRequestSort = (event, property) => {
+    const orderBy = property;
+    let order = 'desc';
+
+    if (this.state.orderBy === property && this.state.order === 'desc') {
+      order = 'asc';
+    }
+
+    this.setState({ order, orderBy });
+  };
+
+  handleClick = (event, id) => {
+  };
+
+  handleChangePage = (event, page) => {
+    this.setState({ page });
+  };
+
+  handleChangeRowsPerPage = event => {
+    this.setState({ rowsPerPage: event.target.value });
+  };
 
   componentWillMount() {
     var that = this;
@@ -31,26 +140,39 @@ class Vets extends Component {
   }
 
   render() {
+
+    const {  order, orderBy, rowsPerPage, page } = this.state;
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
     return (
       <Paper>
         <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell numeric>Firstname</TableCell>
-              <TableCell numeric>Lastname</TableCell>
-              <TableCell numeric>Speciality</TableCell>
-            </TableRow>
-          </TableHead>
+					<EnhancedTableHead
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={this.handleRequestSort}
+              rowCount={rows.length}
+            />
           <TableBody>
-            {this.state.rows.map(row => {
-              return (
-                <TableRow key={row.id}>
-                  <TableCell numeric>{row.firstname}</TableCell>
-                  <TableCell numeric>{row.lastname}</TableCell>
-                  <TableCell numeric>{row.speciality}</TableCell>
-                </TableRow>
-              );
-            })}
+						{stableSort(this.state.rows, getSorting(order, orderBy))
+                .map(n => {
+                  return (
+                    <TableRow
+                      hover
+                      onClick={event => this.handleClick(event, n.id)}
+                      role="checkbox"
+                      tabIndex={-1}
+                      key={n.id}
+                    >
+                      <TableCell padding="checkbox">
+                      </TableCell>
+                      <TableCell component="th" scope="row" padding="none">
+                        {n.firstname}
+                      </TableCell>
+                      <TableCell numeric>{n.lastname}</TableCell>
+                      <TableCell numeric>{n.speciality}</TableCell>
+                    </TableRow>
+                  );
+                })}
           </TableBody>
         </Table>
       </Paper>
