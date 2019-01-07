7c81e8dae5c185fb9a45d43a64931c9f54dab854
Timothee Simon <xorob0@posteo.net>
2019-01-07 23:58:21 +0100

    Corrected a small error

--- src/components/VisitForm.js
+++ src/components/VisitForm.js
@@ -28,7 +28,7 @@ class AddOwnerDialog extends React.Component {
   };
 
 	handleSubmit = (e) => {
-		const url = "http://localhost:9999/api/v1/visitInsert?date=" + this.state.date + "&description=" + this.state.description + "&petId=" + this.state.petId + "&vetId=" + 1;
+		const url = "http://localhost:9999/api/v1/visitInsert?date=" + this.state.date + "&description=" + this.state.description + "&petId=" + this.state.petId + "&vetId=" + this.state.vetId;
 		//TODO date picker
 		//TODO vetid
 		console.log(url);
