5ef7e503e8551cfcb7ddca1f63f7473d029fb119
Timothee Simon <xorob0@posteo.net>
2019-01-07 22:24:24 +0100

    Corrected an other typo

--- src/components/Vet.js
+++ src/components/Vet.js
@@ -81,7 +81,7 @@ class Owner extends Component {
 							value={this.state.speciality}
 							onChange={this.handleChange('speciality')}
 						/>
-						<Button onClick={handleSubmit}>Edit</Button>
+						<Button onClick={this.handleSubmit}>Edit</Button>
 					</form>
 				</Paper>
     );
